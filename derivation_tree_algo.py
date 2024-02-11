07ed0e8654c252f9b30d4ae8c4840a166f47f011
Vishesh Prasad <64453315+VisheshP-29@users.noreply.github.com>
2024-02-10 21:39:08 -0600

    modify accuracy script

--- derivation_tree_algo.py
+++ derivation_tree_algo.py
@@ -14,26 +14,31 @@ def run_derivation_algo():
     algo_most_important_equations = []
     labeled_most_important_equations = []
     article_id_correctness = []
+
+    # Toggle for desired output of algorithm (either a list of equations or a single equation)
+    ret_list = True
     
     print("*-----------------------------------------------------------*")
     print("Important Equation Algorithm Output:")
     for article_id, article in articles.items():
-        cur_most_important_equation = important_equation.get_most_important_equation(article)
-        print(f"Article ID: {article_id} => algo equation(s): {cur_most_important_equation} vs. labeled equation(s): {article['Most Important Equation']}")
-        algo_most_important_equations.append(cur_most_important_equation)
+        cur_most_important_equations = important_equation.get_most_important_equation(article, ret_list)
+        print(f"Article ID: {article_id} => algo equation(s): {cur_most_important_equations} vs. labeled equation(s): {article['Most Important Equation']}")
+        algo_most_important_equations.append(cur_most_important_equations)
         labeled_most_important_equations.append(article['Most Important Equation'])
         article_id_correctness.append(article_id)
+    
+    algo_accuracy, algo_precision, algo_recall, algo_f1_score, algo_articles_used = important_equation.get_algo_correctness(labeled_most_important_equations, algo_most_important_equations, article_id_correctness, ret_list)
 
-    algo_conf_matrix, algo_accuracy, algo_precision, algo_recall, algo_specificity, algo_f1_score, algo_articles_used = important_equation.get_algo_correctness(labeled_most_important_equations, algo_most_important_equations, article_id_correctness)
     print("*-----------------------------------------------------------*")
     print("Important Equation Algorithm Correctness: ")
     print(f"Articles used for correctness calculations: {algo_articles_used}")
     print(f"Accuracy: {algo_accuracy:.8f}")
     print(f"Precision: {algo_precision:.8f}")
     print(f"Recall: {algo_recall:.8f}")
-    print(f"Specificity: {algo_specificity:.8f}")
     print(f"F1 Score: {algo_f1_score:.8f}")
     print("*-----------------------------------------------------------*")
+
+
 
 """
 Entry point for derivation_tree_algo.py
