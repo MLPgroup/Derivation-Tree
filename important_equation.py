8d23fe1a342c1bece860123971eb470ea3af60df
Vishesh Prasad <64453315+VisheshP-29@users.noreply.github.com>
2024-04-28 18:52:52 -0500

    minor fixes

--- important_equation.py
+++ important_equation.py
@@ -20,7 +20,7 @@ Input: article -- dictionary with following values:
                     - Article ID: (string)
                     - Equation ID: (list of strings)
                     - Adjacency List (outgoing edges for each node, dict with key = string and value = list of strings)
-       ret_list -- return either a list of equations or a single equation
+       ret_list -- boolean which indicates to return either a list of equations or a single equation
 Return: string -- most important equation in the article
 Function: run a custom algorithm on the article to find the most important equation
 Note: (1) The following algorithm works on a directed, acyclic graph
@@ -48,14 +48,14 @@ def get_most_important_equation(article, ret_list):
         num_outgoing[equation] += (num_outgoing_bias if adjacency_list[equation][0] != None else 0)
 
         # Get starting nodes:
-        # No outgoing edges
+        # Filter for no outgoing edges
         if adjacency_list[equation][0] == None:
             if equation in start_nodes:
                 start_nodes.remove(equation)
-        # No incoming edge
-        for outgoing_edge in adjacency_list[equation]:
-            if outgoing_edge in start_nodes:
-                start_nodes.remove(outgoing_edge)
+        # Filter for no incoming edge
+        for child_node in adjacency_list[equation]:
+            if child_node in start_nodes:
+                start_nodes.remove(child_node)
 
     # Start node manipulation for consistency
     start_nodes = list(start_nodes)
@@ -102,6 +102,8 @@ def get_most_important_equation(article, ret_list):
             if adjacency_list[cur_node][0] != None:
                 node_weight = cur_node_weights[cur_node]
                 tot_outgoing = sum(num_outgoing[child_node] for child_node in adjacency_list[cur_node])
+                if tot_outgoing == 0:
+                    continue
                 # Add weights for all children
                 for child_node in adjacency_list[cur_node]:
                     queue.appendleft(child_node)
